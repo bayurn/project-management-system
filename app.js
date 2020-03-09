e6cb20248d00e214068b524eb9078045492741f6
bayurn <bayurizkynurseto19@gmail.com>
2020-03-09 22:53:38 +0700

    Recheck app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ 
-  secret: 'nuaing'
+  secret: 'keyboard cat'
 }));
 app.use(flash());
 
